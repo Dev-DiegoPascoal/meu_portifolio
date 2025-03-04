b71f0317239f24ebd808b1700dc77970b94b870e
Diego Ap. Viola Pascoal <90094703+Dev-DiegoPascoal@users.noreply.github.com>
2025-03-04 00:03:42 -0300

    Alteração no footer e nos arquivos

--- meus_contatos.php
+++ meus_contatos.php
@@ -82,16 +82,7 @@
         </div>
     </body>
 
-    <footer class="footer">
-    <footer class="footer">
-        <div class="footer-content">
-            <p>Desenvolvido por: <img src="src/imagens/logo_diegopascoal_branca_pequeno.png" alt=""></p>
-            <p><a href="https://github.com/Dev-DiegoPascoal" target="_blank">github.com/Dev-DiegoPascoal</a></p>
-            <p><a href="https://www.linkedin.com/in/diegoappascoal/" target="_blank">linkedin.com/in/diegoappascoal</a></p>
-            <p>Versão: 1.0.0</p>
-            <p id="date-time"></p>
-        </div>
-    </footer>
+    <?php include 'footer.php'; ?>
     <script src="./src/JS/data-hora.js"></script>
     <script src="./src/JS/carrossel-imagens.js"></script>
     
